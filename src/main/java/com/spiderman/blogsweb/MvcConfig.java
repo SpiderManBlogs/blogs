fe6709a6fdb40eb4056e01420e1efd796fcdac7a
dazhang12138 <836160973da@gmail.com>
2022-05-18 14:18:25 +0800

    修改日志配置+修改启动访问路径+SpringBoot跨域请求配置

---
+++ src/main/java/com/spiderman/blogsweb/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.spiderman.blogsweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST","GET")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
